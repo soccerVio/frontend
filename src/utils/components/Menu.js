62f2b4ef2ed594cef8de659bde15723103f29b47
krach-achraf <krachachraf98@gmail.com>
2022-12-02 21:04:15 +0100

    update menu

--- src/utils/components/Menu.js
+++ src/utils/components/Menu.js
@@ -8,54 +8,26 @@ import "./Menu.css";
 
 const Menu = () => {
   return (
-    <div className="menu">
-      <nav className="vertMenu">
-        <div className="vertMenu-logo">SoccerVio</div>
-        <div className="vertMenu-items">
-          <div className="vertMenu-item">
-            <BiHome className="vertMenu-icon" />
-            <span>Home</span>
-          </div>
-          <div className="vertMenu-item">
-            <BiCalendar className="vertMenu-icon" />
-            <span>Reservations</span>
-          </div>
-          <div className="vertMenu-item">
-            <MdOutlineReportProblem className="vertMenu-icon" />
-            <span>Reports</span>
-          </div>
-          <div className="vertMenu-item">
-            <BiUser className="vertMenu-icon" />
-            <span>Users</span>
-          </div>
-          <div className="vertMenu-item">
-            <TbMapSearch className="vertMenu-icon" />
-            <span>Searches</span>
-          </div>
-          <div className="vertMenu-item">
-            <TbSoccerField className="vertMenu-icon" />
-            <span>Stades</span>
-          </div>
-          <div className="vertMenu-item">
-            <TfiAnnouncement className="vertMenu-icon" />
-            <span>Announcements</span>
-          </div>
-        </div>
+    <header className="header">
+      <div className="header-logo">SoccerVio</div>
+      <nav className="menu">
+        <BiHome className="menu-icon" />
+        <BiCalendar className="menu-icon" />
+        <MdOutlineReportProblem className="menu-icon" />
+        <TbMapSearch className="menu-icon" />
+        {/*<BiUser className="menu-icon" />
+        <TbSoccerField className="menu-icon" />
+        <TfiAnnouncement className="menu-icon" />*/}
       </nav>
-      <header className="horizMenu">
-        <div className="horizMenu-authUser">
-          <img src="/images/userImage.jpg" className="horizMenu-imgUser" />
-          <span>Krach achraf</span>
+      <div className="header-rightSide">
+        <div className="header-authUser">
+          <img src="/images/userImage.jpg" className="header-imgUser" />
+          <span className="header-nameUser">Krach achraf</span>
         </div>
-        <div className="horizMenu-rightSide">
-          <MdNotifications className="horizMenu-notifIcon" />
-          <div className="horizMenu-logout">
-            <span>Log out</span>
-            <FiLogOut className="horizMenu-logoutIcon" />
-          </div>
-        </div>
-      </header>
-    </div>
+        <MdNotifications className="header-notifIcon" />
+        <FiLogOut className="header-logoutIcon" />
+      </div>
+    </header>
   );
 };
 
